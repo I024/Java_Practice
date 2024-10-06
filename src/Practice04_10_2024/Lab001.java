b3e9262b043992741dd3ed5d8499f0c374078da5
Imran Dasadiya <pimran738@gmail.com>
2024-10-06 23:59:30 +0530

    All Files Commit

--- src/Practice04_10_2024/Lab001.java
+++ src/Practice04_10_2024/Lab001.java
@@ -1,4 +1,16 @@
 package Practice04_10_2024;
 
-public class Lab001 {
+class Calculator {
+    public static void main (String[] args) {
+        int a = 40;
+        int b = 20;
+        System.out.println("A Is: " + a );
+        System.out.println("B Is: " + b );
+        System.out.println("Addition Of A + B Is: " + (a + b));
+        System.out.println("Subtraction Of A - B Is: " + (a - b));
+        System.out.println("Division Of A / B Is: " + (a / b));
+        System.out.println("Multiplication Of A * B Is: " + (a * b));
+    }
 }
+
+
