553b4d402646f9f319e758da69931b97ec2303b8
Imran Dasadiya <pimran738@gmail.com>
2024-10-10 00:17:23 +0530

    All Files Commit

---
+++ src/Practice09_10_2024/Table.java
@@ -0,0 +1,27 @@
+package Practice09_10_2024;
+
+public class Table {
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.printf("%d * %d = %d\n", a,1, a);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,2,a*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,3,a*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,4,a*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,5,a*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,6,a*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,7,a*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,8,a*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,9,a*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d\n", a,10,a*10);
+
+    }
+}
